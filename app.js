1511cd382c557a32020a91e44b76b6ee41717a3f
Simon Kusterer <simon@soped.com>
2014-04-20 10:54:01 +0200

    piping seems to work for us again in newer node versions

--- app.js
+++ app.js
@@ -2,9 +2,6 @@ var http = require('http')
   , fs = require('fs')
   , indexHtml = fs.readFileSync('./public/index.html')
   , BinaryServer = require('binaryjs').BinaryServer
-
-    // WebSocket Server listens on port 9000
-
   , binaryServer = BinaryServer({port: 9000});
 
 // Serv the static page..
@@ -25,12 +22,7 @@ binaryServer.on('connection', function(client) {
   client.on('stream', function(stream) {
 
       var responseStream = client.createStream('fromserver');
-
-      // pipe didnt work :(
-
-      stream.on('data', function(data) {
-        responseStream.write(data);
-      });
+      stream.pipe(responseStream);
 
   });
 
